b0515e500bfe6a106638c5c680b1cf34e28d0901
Teresa Malave <dev.malave7@gmail.com>
2020-03-28 20:03:20 -0500

    thinking of making buttons into components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="App">
+  
+        <div className="button-container">
+
+          {/* <Start />
+          <Pause />
+          <Restart /> */}
+
+        </div>
+
       </div>
     )
   }
